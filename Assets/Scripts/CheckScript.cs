30fe1d59eb5c1c8595b2d72852a048cdd6df8ff8
AlexBerg85 <al-berg85@hotmail.com>
2017-11-26 01:51:16 +0100

    Learning to Code with Unity

---
+++ Assets/Scripts/CheckScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckScript : MonoBehaviour {
+
+	private float difficulty;
+
+	// Use this for initialization
+	void Start () {
+		difficulty = PlayerPrefsManager.GetDifficulty ();
+		Debug.Log (difficulty);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelAfter;
+
+	void Start () {
+		if (autoLoadNextLevelAfter <= 0) {
+			Debug.Log ("Level autoload disabled");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level Load requestet for: "+name);
+        SceneManager.LoadScene(name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit Game Requestet");
+		Application.Quit();
+	}
+
+    
+
+
+	public void LoadNextLevel() {
+        SceneManager.LoadScene(+1);
+	}
+}
